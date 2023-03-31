d2c353b97ce912bf5f2dafd9fdfe4814497e168b
Noman <maliknomi1032@gmail.com>
2023-03-31 10:37:03 -0400

    Code refactored

--- MyRide/MyRide/Program.cs
+++ MyRide/MyRide/Program.cs
@@ -59,27 +59,34 @@ namespace MyRide
                     newRide.AssignPassenger(new Passenger(name, phoneNo));
                     newRide.GetLocations(startLocation, endLocation);
 
-                    Driver nearestAvailableDriver = SearchNearestAvailableDriver(startLocation);
-                    newRide.AssignDriver(nearestAvailableDriver);
-
-                    int price = newRide.CalculatePrice(rideType);
-                    Console.WriteLine("-------------------- THANK YOU ------------------");
-                    Console.WriteLine("Price for this ride is: " + price);
-                    Console.Write("Enter ‘Y’ if you want to Book the ride, enter ‘N’ if you want to cancel operation: ");
-                    string confirmation = Console.ReadLine();
-
-                    if (confirmation.ToUpper() == "Y")
+                    Driver nearestAvailableDriver = SearchNearestAvailableDriver(startLocation, rideType);
+                    if(nearestAvailableDriver != null)
                     {
-                        newRide.passenger.BookRide();
-                        newRide.passenger.GiveRating();
-                        Console.WriteLine("Happy Travel...!");
+                        newRide.AssignDriver(nearestAvailableDriver);
+
+                        int price = newRide.CalculatePrice(rideType);
+                        Console.WriteLine("-------------------- THANK YOU ------------------");
+                        Console.WriteLine("Price for this ride is: " + price);
+                        Console.Write("Enter ‘Y’ if you want to Book the ride, enter ‘N’ if you want to cancel operation: ");
+                        string confirmation = Console.ReadLine();
+
+                        if (confirmation.ToUpper() == "Y")
+                        {
+                            newRide.passenger.BookRide();
+                            newRide.passenger.GiveRating();
+                            Console.WriteLine("Happy Travel...!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ride cancelled.");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Ride cancelled.");
+                        Console.WriteLine("No driver available with "+ rideType);
                     }
 
-                    static Driver SearchNearestAvailableDriver(Location startLocation)
+                    static Driver SearchNearestAvailableDriver(Location startLocation, string rideType)
                     {
                         // Assume drivers is a list of available drivers
                         List<Driver> drivers = new List<Driver>();
@@ -94,7 +101,7 @@ namespace MyRide
 
                         foreach (Driver driver in drivers)
                         {
-                            if (driver.Availability)
+                            if (driver.Availability && driver.Vehicle.Type.ToLower() == rideType.ToLower())
                             {
                                 double distance = Location.CalculateDistance(startLocation, driver.CurrLocation);
                                 if (distance < smallestDistance)
@@ -215,184 +222,5 @@ namespace MyRide
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//using RideHailingApp.Driver;
-
-////static List<Driver> drivers = new List<Driver>();
-
-//namespace MyRide
-//{
-//    class Program
-//    {
-//        static List<Driver> drivers = new List<Driver>();
-
-
-
-//        static void Main(string[] args)
-//        {
-//            drivers.Add(new Driver(3, "John Doe", 30, "Male", "123 Main St", "555-1234", new Location(37.7749, -122.4194), new Vehicle("Toyota", "Corolla"), new List<int> { 4, 5, 3 }, true));
-//            drivers.Add(new Driver(4, "Jane Smith", 25, "Female", "456 Elm St", "555-5678", new Location(37.7739, -122.4312), new Vehicle("Honda", "Accord"), new List<int> { 5, 5, 4 }, false));
-
-//            Console.WriteLine("-------------------------------------------");
-//            Console.WriteLine("             WELCOME TO MYRIDE");
-//            Console.WriteLine("-------------------------------------------");
-//            Console.WriteLine("1. Book a Ride");
-//            Console.WriteLine("2. Enter as Driver");
-//            Console.WriteLine("3. Enter as Admin");
-
-
-//            Console.WriteLine("Press 1 to 3 to select an option:");
-
-//            int option = Convert.ToInt32(Console.ReadLine());
-
-//            if (option == 2)
-//            {
-//                Console.WriteLine("-------------------------------------------");
-//                Console.WriteLine("            WELCOME TO Driver Portal");
-//                Console.WriteLine("-------------------------------------------");
-
-//                // Prompt the user to enter their ID and name
-//                Console.WriteLine("Enter Your ID: ");
-//                int id = Convert.ToInt32(Console.ReadLine());
-//                Console.WriteLine("Enter Your Name: ");
-//                string name = Console.ReadLine();
-
-//                // Check if the entered ID and name match a driver in the list
-//                Driver driver = drivers.FirstOrDefault(d => d.ID == id && d.Name.ToLower().Trim() == name.ToLower().Trim());
-//                if (driver != null)
-//                {
-//                    Console.WriteLine("Hello " + driver.Name + "!");
-//                    bool exit = false;
-
-//                    // Loop until the driver chooses to exit
-//                    do
-//                    {
-//                        Console.WriteLine("1. Change availability ");
-//                        Console.WriteLine("2. Change Location ");
-//                        Console.WriteLine("3. Exit as Driver ");
-//                        Console.WriteLine("Choose one from above options.");
-
-//                        option = Convert.ToInt32(Console.ReadLine());
-
-//                        switch (option)
-//                        {
-//                            case 1:
-//                                Console.WriteLine("Are you available? Enter a boolean value (True or False):");
-//                                string userInput = Console.ReadLine();
-//                                bool myBool = bool.Parse(userInput);
-//                                driver.UpdateAvailability(myBool);
-//                                break;
-//                            case 2:
-//                                Console.WriteLine("Update your Location: ");
-//                                string[] locationInput = Console.ReadLine().Split(',');
-//                                double x = double.Parse(locationInput[0].Trim());
-//                                double y = double.Parse(locationInput[1].Trim());
-//                                Location currentLocation = new Location(x, y);
-//                                driver.UpdateLocation(currentLocation);
-//                                break;
-//                            case 3:
-//                                exit = true;
-//                                break;
-//                            default:
-//                                Console.WriteLine("Invalid option!");
-//                                break;
-//                        }
-
-//                    } while (!exit);
-//                }
-//                else
-//                {
-//                    Console.WriteLine("Error! You are not an authenticated user of this application");
-//                }
-//            }
-
-
-//            //Console.WriteLine(option);
-//            //if (option == 2)
-//            //{
-//            //    Console.WriteLine("-------------------------------------------");
-//            //    Console.WriteLine("            WELCOME TO Driver Portal");
-//            //    Console.WriteLine("-------------------------------------------");
-
-//            //    Console.WriteLine("Enter Your ID: ");
-//            //    int id = Convert.ToInt32(Console.ReadLine());
-//            //    Console.WriteLine("Enter Your Name: ");
-//            //    string name = Console.ReadLine();
-
-//            //    if(drivers.Count> 0)
-//            //    {
-//            //        foreach (Driver driver in drivers)
-//            //        {
-//            //            if (driver.ID == id && driver.Name.ToLower().Trim() == name.ToLower().Trim())
-//            //            {
-//            //                Console.WriteLine("Hello " + driver.Name + "!");
-
-//            //                Console.WriteLine("Enter your current Location: ");
-
-//            //                string[] locationInput = Console.ReadLine().Split(',');
-//            //                double x = double.Parse(locationInput[0].Trim());
-//            //                double y = double.Parse(locationInput[1].Trim());
-//            //                Location currentLocation = new Location(x,y);
-//            //                //currentLocation.Latitude = x;
-//            //                //currentLocation.Longitude = y;
-
-//            //                Console.WriteLine("1. Change availability ");
-//            //                Console.WriteLine("2. Change Location ");
-//            //                Console.WriteLine("3. Exit as Driver ");
-
-
-//            //                Console.WriteLine("Choose one from above options.");
-
-//            //                option = Convert.ToInt32(Console.ReadLine());
-
-//            //                if (option == 1)
-//            //                {
-//            //                    Console.WriteLine("Are you availble? Enter a boolean value (True or False):");
-//            //                    string userInput = Console.ReadLine();
-//            //                    bool myBool = bool.Parse(userInput);
-//            //                    driver.UpdateAvailability(myBool);
-//            //                }
-//            //                else if (option == 2)
-//            //                {
-//            //                    Console.WriteLine("Update your Location: ");
-
-//            //                    locationInput = Console.ReadLine().Split(',');
-//            //                    x = int.Parse(locationInput[0].Trim());
-//            //                    y = int.Parse(locationInput[1].Trim());
-//            //                    currentLocation.Latitude = x;
-//            //                    currentLocation.Longitude = y;
-//            //                }
-//            //                else if(option == 3)
-//            //                {
-
-//            //                }
-//            //            }
-//            //        }
-//            //    }
-//            //    else
-//            //    {
-//            //        Console.WriteLine("Error! You are not authenticated user of this application");
-
-//            //    }
-//        }
-//    }
-//    }
-//}
 
 
